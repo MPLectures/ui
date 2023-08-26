aa57ed90d1e2a2da7173f68da550bf96fa47d641
MPLectures <microprogramerszoom@gmail.com>
2023-08-26 16:06:42 +0500

    second lecture widget tree fonts and images

--- lib/views/screens/screen_home.dart
+++ lib/views/screens/screen_home.dart
@@ -6,7 +6,35 @@ class ScreenHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Colors.teal,
+      backgroundColor: Colors.pink,
+      // body: Center(
+      //   child: Container(
+      //     height: 150,
+      //     width: 300,
+      //     color: Color(0xffba68c8),
+      //     child: Center(
+      //       child: Text(
+      //         'Wow',
+      //         style: TextStyle(fontFamily: 'TiltPrism'),
+      //       ),
+      //     ),
+      //   ),
+      // ),
+      body: Center(
+        child: Container(
+            height: 600,
+            width: 500,
+            color: Colors.white,
+           child:
+          Image.asset(
+            // height: 100,
+            // width: 300,
+            fit: BoxFit.fill,
+            'assets/images/pexels-abdel-achkouk-16361788.jpg',
+          ),
+        ),
+      ),
     );
   }
 }
+
